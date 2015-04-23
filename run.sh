b72e3cfd2d9790b2f144a8dd0b4dec9743a0365f
aviau <alexandre.viau@savoirfairelinux.com>
2015-04-23 11:18:51 -0400

    Don't create data sources multiple times

--- run.sh
+++ run.sh
@@ -9,9 +9,12 @@ function create_db {
     curl --cookie /tmp/cookiefile 'http://localhost/api/datasources' -X PUT -H 'Content-Type: application/json;charset=utf-8' --data '{"name":"influxdb","type":"influxdb","url":"http://'${INFLUXDB_HOST}':'${INFLUXDB_PORT}'","access":"proxy","isDefault":true,"database":"'${INFLUXDB_NAME}'","user":"'${INFLUXDB_USER}'","password":"'${INFLUXDB_PASS}'"}'
 
     echo -e "Created InfluxDB datasources."
-
 }
 
-(sleep 60 && create_db)&
+if [ -f "/.pre_db_created" ]; then
+    echo "=> Data sources had been created before, skipping ..."
+else
+    (sleep 60 && create_db && touch "/.pre_db_created")&
+fi
 
  ./bin/grafana-server -config="/grafana.ini"
